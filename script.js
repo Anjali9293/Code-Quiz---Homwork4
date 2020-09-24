227efd3796c4bb2c63f09c30c5f4968986c71305
Anjali Pant <pantanjali7@gmail.com>
2020-09-23 22:00:32 -0400

    submit high score and display high score

--- script.js
+++ script.js
@@ -57,7 +57,13 @@ var myQuestions = [
 ];
 
 var elements = {
-  welcome: document.querySelector("#Welcome"),
+  page: {
+    Welcome: document.querySelector("#WelcomePage"),
+    Question: document.querySelector("#QuestionPage"),
+    Results: document.querySelector("#ResultsPage"),
+    Highscore: document.querySelector("#HighscorePage"),
+    ViewHighscore: document.querySelector("#HighscoreViewPage")
+  },
   question: document.querySelector("#question-container"),
   description: document.querySelector("#description"),
   options: {
@@ -67,15 +73,20 @@ var elements = {
     c: document.querySelector("#c"),
     d: document.querySelector("#d")
   },
-  results: document.querySelector("#result"),
+  message: document.querySelector("#message"),
   start: document.querySelector("#start"),
-  timer: document.querySelector("#countDown")  
+  timer: document.querySelector("#countDown"),
+  Finalscore: document.querySelector("#score"), 
+  UserInitials: document.querySelector("#initials"),
+  SubmitScore: document.querySelector("#submitScore"),
+  HighscoreList: document.querySelector("#HighscoreList")
 };
 
 var timer;
 var timerCount = 60;
 var questionIndex = 0;
 var scoreCount = 0;
+var highscores = JSON.parse(localStorage.highscores) || [];
 
 function initiateQuiz() {
   timer = setInterval(startTimer, 1000);
@@ -99,11 +110,8 @@ function endTimer() {
 }
 
 function prepareQuiz() {
-  elements.start.classList.add("hide");
-  elements.welcome.classList.add("hide");
-  elements.description.classList.add("hide");
-  elements.question.classList.remove("hide");
-  elements.options.all.classList.remove("hide");
+  elements.page.Welcome.classList.add("hide");
+  elements.page.Question.classList.remove("hide");
 }
 
 function displayQuestion() {
@@ -115,32 +123,73 @@ function displayQuestion() {
   prepareQuiz();
 }
 
-function displayResults(result) {
-  elements.results.textContent = result;
-  elements.results.classList.remove("hide");
-  setTimeout(hideResults, 1000);
+function displayMessage(result) {
+  elements.message.textContent = result;
+  elements.message.classList.remove("hide");
+  setTimeout(hideMessage, 1000);
 }
 
-function hideResults() {
-  elements.results.classList.add("hide");  
+function hideMessage() {
+  elements.message.classList.add("hide");  
 }
 
 function checkAnswer(event) {
   if (event.target.id == myQuestions[questionIndex].correctAnswer) {      
     myQuestions[questionIndex].correct = true;
-    displayResults("Correct!")  
-  } else displayResults("wrong!"); 
+    displayMessage("Correct!")  
+  } else displayMessage("wrong!"); 
 
   nextQuestion();
 }
 
 function nextQuestion() {
   questionIndex++;
-  displayQuestion(); 
+  if(myQuestions[questionIndex]) displayQuestion();
+  else displayResults(); 
+}
+
+function displayResults() {
+  elements.page.Results.classList.remove("hide");
+  elements.page.Question.classList.add("hide");
+
+  var score = myQuestions.map(function(question,index) {
+    return question.correct;
+  }).reduce((a, b) => a + b, 0);
+  var percentage = score/myQuestions.length*100;
+  elements.Finalscore.textContent = percentage+"%";
+  
+}
+
+function scoreSubmit(){
+  highscores.push({
+    initials: elements.UserInitials.value,
+    score: elements.Finalscore.textContent
+  });
+  highscores = highscores.sort(function(a, b){
+    var first = parseInt(b.score.replace("%", ""));
+    var second = parseInt(a.score.replace("%", ""));
+    return first-second
+  });
+  localStorage.highscores = JSON.stringify(highscores);
+  elements.UserInitials.value = "";
+  displayHighscore();
+}
+
+function displayHighscore() {
+  elements.page.Results.classList.add("hide");
+  elements.page.Highscore.classList.remove("hide");  
+
+  highscores.forEach(function(score, i) {
+    var highscoreStr = (i+1) + ". " + score.initials + " - " + score.score;
+    var node = document.createElement("li");               
+    var textnode = document.createTextNode(highscoreStr);        
+    node.appendChild(textnode);                             
+    elements.HighscoreList.appendChild(node);     
+  });
+
 }
 
 elements.start.addEventListener("click", initiateQuiz);
 elements.options.all.addEventListener("click", checkAnswer);
-
-
+elements.SubmitScore.addEventListener("click",scoreSubmit);
 
